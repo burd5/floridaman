32f09418b0389bd58ababd380ea80e016d1f1ba6
burd5 <austinburdette5@gmail.com>
2023-02-13 17:26:57 -0700

    added news headline banner and edited instructions modal

--- src/Pages/Username.jsx
+++ src/Pages/Username.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import '../Styles/username.css'
+import gator from '../Images/gator.jpeg'
 
 export default function Username() {
   return (
     <div className="username">
+        <div className="scroll">
+          <div class="m-scroll">
+            <span>FLORIDA MAN FOUND WITH DRUGS AFTER GETTING TRAPPED IN PORT-A-POTTY</span>
+            <span><img className="gatorIcon" src={gator} alt="" /></span>
+            <span className="blueU">AFTER POLICE CHASE, FLORIDA WOMAN CROSSES 'GETTING ARRESTED' OFF BUCKET LIST, DEPUTIES SAY</span>
+            <span><img className="gatorIcon" src={gator} alt="" /></span>
+            <span>FLORIDA MAN CHARGED FOR THROWING HOT DOG AT ST. PETE POLICE OFFICER</span>
+            <span><img className="gatorIcon" src={gator} alt="" /></span>
+            <span span className="blueU">FLORIDA MAN DRIVES STOLEN TRUCK TO SPACE FORCE BASE TO WARN OF BATTLE BETWEEN ALIENS, DRAGONS</span>
+            <span><img className="gatorIcon" src={gator} alt="" /></span>
+            <span>DEPUTIES RELEASE VIDEO OF FLORIDA WOMAN IRISH FOLK DANCING DURING SOBRIETY TESTS</span>
+            <span><img className="gatorIcon" src={gator} alt="" /></span>
+            <span span className="blueU">FLORIDA MAN WEARING NOTHING BUT COWBOY HAT ATTACKS WOMAN WITH MACHETE</span>
+            <span><img className="gatorIcon" src={gator} alt="" /></span>
+            <span>FLORIDA WOMAN SPEEDS THROUGH CHECKPOINT, TAKES SELFIE DURING TRAFFIC STOP</span>
+            <span><img className="gatorIcon" src={gator} alt="" /></span>
+            <span span className="blueU">FLORIDA MAN CHARGED WITH ASSAULT WITH A DEADLY WEAPON AFTER THROWING ALLIGATOR THROUGH WENDY'S DRIVE-THRU WINDOW</span>
+            <span><img className="gatorIcon" src={gator} alt="" /></span>
+            <span>FLORIDA COP CLAIMS BURGER KING PUT DIRT ON HIS FOOD - INVESTIGATION REVEALS IT WAS SEASONING </span>
+            <span><img className="gatorIcon" src={gator} alt="" /></span>
+            <span span className="blueU">FLORIDA MEN USE SPAGHETTI SAUCE IN BURGLARY PLOT </span>
+          </div>
+        </div>
         <form className="userForm" action="">
           <label htmlFor="">Enter a player name to continue</label>
           <input type="text" />
--- src/Pages/landing.jsx
+++ src/Pages/landing.jsx
@@ -46,10 +46,12 @@ export default function Landing() {
         <div className="modal">
             <h1 className="instructions">Instructions</h1>
             <ul className='rulesList'>
-                <li className="ruleItems">Read the news headline and decide which US state it originated from</li>
-                <li className="ruleItems">Choose your difficulty level - easy, medium, or hard</li>
+                <li className="ruleItems">Click on <img onClick={displayLeaderboard} className="treeListIcon" src={tree} alt="palm tree" /> to view the leaderboards</li>
+                <li className="ruleItems">Click on <i onClick={displayAnalytics}className="analyticsListIcon fa-solid fa-chart-simple"></i> to view analytics for specific questions</li>
+                <li className="ruleItems">To begin a game, enter a player name and hit start</li> 
+                <li className="ruleItems">Choose a difficulty level - Easy, Medium, or Hard</li> 
+                <li className="ruleItems">Read the news headline and guess which US state it's from</li>
                 <li className="ruleItems">After three wrong answers, the game is over</li>
-                <li className="ruleItems">Click on the palm tree to view the leaderboards and the bar graph to view analytics for specific questions</li>
             </ul>
         </div>
         </Box>
